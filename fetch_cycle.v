ff4aaac085d13449995736a419dd305cca851fb5
Vinayak Prakash <vinayakprakash2121@gmail.com>
2025-03-12 18:32:07 +0530

    ss

--- fetch_cycle.v
+++ fetch_cycle.v
@@ -1,20 +1,22 @@
 module fetch_cycle (
     input clk,rst,PCSrcE,
-    input [31:0] PCTragetE,
+    input [31:0] PCTargetE,
     output [31:0] InstrD,PCD,PCPlus4D,
     input StallF,StallD,FlushD,
     output Predict_branchD,
     input Eval_branch
 );
-wire [31:0] PC_F,PCF,PCPlus4F,InstrF;
+wire [31:0] PC_F,PCF,PCPlus4F,InstrF,Target_finalF;
 wire Predict_branchF;
 reg [31:0] InstrF_reg,PCF_reg,PCPlus4F_reg;
 reg Predict_branchF_reg;
 
-mux2 pcmux(PCPlus4F,PCTragetE,PCSrcE,PC_F);
+mux2 pcmux(PCPlus4F,Target_finalF,Predict_branchF,PC_F);
 reset_ff PC(clk,rst,PC_F,PCF,StallD);
 instr_mem im(PCF,InstrF);
 adder pcadder(PCF,32'h4,PCPlus4F);
+//predictor
+branch_predictor bp(clk,rst,Eval_branch,PCSrcE,PCTargetE,InstrF,PCF,Predict_branchF,Target_finalF);
 
 always @(posedge clk or posedge rst) begin
     if(rst==1'b1 | FlushD)begin
